83231411cf6de22b822ec2adafd343dd95885c65
Devgochan <devgochan@gmail.com>
2024-02-01 04:23:19 +0900

    8958, 10809 완

--- GHC/week2/2557.py
+++ GHC/week2/2557.py
@@ -1,9 +1,9 @@
-list_ = [0] * 10
+list_ = [0] * 10 # 초기화 안해주면 index error 발생함
 a = int(input())
 b = int(input())
 c = int(input())
 
-str_ = str(a * b * c)
+str_ = str(a * b * c) # 정수를 문자열로 변환
 
 for i in str_:
     list_[int(i)] += 1
