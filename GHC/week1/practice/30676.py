05822a596ad9e4b0e54caab6745ab26173614099
Devgochan <devgochan@gmail.com>
2024-01-31 20:03:33 +0900

    study.md upload

---
+++ GHC/week1/practice/30676.py
@@ -0,0 +1,16 @@
+x = int(input())
+
+if x >= 620 and x <= 780 :
+    print('Red')
+elif x >= 590 and x <= 620:
+    print('Orange')
+elif x >= 570 and x <= 590:
+    print('Yellow')
+elif x >= 495 and x <= 570:
+    print('Green')
+elif x >= 450 and x <= 495:
+    print('Blue')
+elif x >= 425 and x <= 450:
+    print('Indigo')
+elif x >= 380 and x <= 425:
+    print('Violet')
