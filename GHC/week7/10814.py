5d171100d1a549fd1deae895fcbd49223bb85521
Devgochan <devgochan@gmail.com>
2024-03-28 16:16:18 +0900

    g

--- GHC/week7/10814.py
+++ GHC/week7/10814.py
@@ -1,8 +1,12 @@
 n = int(input())
+member_lst = []
 
-members = []
+for i in range(n):
+    age, name = map(str, input().split())
+    age = int(age)
+    member_lst.append((age, name))
 
-for _ in range(n) :
-    members.append(input().split())
+member_lst.sort(key = lambda x : x[0])	## (age, name)에서 age만 비교
 
-print(members)
+for i in member_lst:
+    print(i[0], i[1])
